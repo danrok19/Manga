eb11ff8b1c96c5502d3e73305692dd43936ac4d3
Kobe <danrok19@wp.pl>
2024-12-14 19:13:07 +0100

    Przerzut do package dao.sql

--- src/main/java/com/db/manga/dao/sql/ChapterRatingRepository.java
+++ src/main/java/com/db/manga/dao/sql/ChapterRatingRepository.java
@@ -1,4 +1,4 @@
-package com.db.manga.dao;
+package com.db.manga.dao.sql;
 
 import com.db.manga.entity.sql.ChapterRating;
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/db/manga/dao/sql/MangaRatingRepository.java
+++ src/main/java/com/db/manga/dao/sql/MangaRatingRepository.java
@@ -1,4 +1,4 @@
-package com.db.manga.dao;
+package com.db.manga.dao.sql;
 
 import com.db.manga.entity.sql.MangaRating;
 import org.springframework.data.jpa.repository.JpaRepository;
