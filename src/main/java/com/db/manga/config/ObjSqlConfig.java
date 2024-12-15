7bcd5ab3ad2c8bbcff257bc8525b739da1f863ba
Kobe <danrok19@wp.pl>
2024-12-15 16:07:02 +0100

    configs dla rel i obj-rel db

---
+++ src/main/java/com/db/manga/config/ObjSqlConfig.java
@@ -0,0 +1,48 @@
+package com.db.manga.config;
+
+
+import com.zaxxer.hikari.HikariDataSource;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.db.manga.dao.objsql")
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.db.manga.entity.objsql.model")
+public class ObjSqlConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        HikariDataSource dataSource = new HikariDataSource();
+
+        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/dbmangav2");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("Zoltanlubi1$");
+
+        return dataSource;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder, DataSource dataSource){
+        return builder
+                .dataSource(dataSource)
+                .packages("com.db.manga.entity.objsql.model")
+                .persistenceUnit("manga")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
